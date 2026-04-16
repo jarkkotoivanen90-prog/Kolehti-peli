7e67598ec0f6527a9fad552a590d82f7ad20ca23
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-16 20:18:15 +0300

    Rename root variable and simplify render method

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-const root = document.getElementById("root");
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(rootElement).render(<App />);

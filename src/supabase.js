f794f6b60f395acacaee077bed13e002528b1447
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-16 20:48:13 +0300

    Initialize Supabase client in supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =https://garqbefezreppqdheocw.supabase.co
+const supabaseKey = sb_publishable_VehM8pJcRzFJO3-ZQd3IeA_YQCqqqES
+
+export const supabase = createClient(supabaseUrl, supabaseKey);

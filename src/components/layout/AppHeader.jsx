22fb8bd8993f175a9c49d0a639ddda98f0706bb8
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-19 18:31:11 +0300

    Format email display in AppHeader component

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -4,10 +4,14 @@ export default function AppHeader({ email }) {
       <div className="text-sm font-black tracking-[0.3em] text-white/80">
         KOLEHTI
       </div>
+
       <div className="mt-2 text-5xl font-black leading-none md:text-7xl">
         Kolehti AI
       </div>
-      <div className="mt-3 text-lg text-white/75">{email}</div>
+
+      <div className="mt-3 text-lg text-white/75">
+        {email}
+      </div>
     </div>
   );
 }

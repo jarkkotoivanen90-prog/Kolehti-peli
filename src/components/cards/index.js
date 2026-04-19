8608a74a98527b2da3abbeed1b2ed4ae8c05ae84
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-19 18:01:25 +0300

    Export card components from index.js

---
+++ src/components/cards/index.js
@@ -0,0 +1,4 @@
+export { default as PrimaryActionCard } from "./PrimaryActionCard";
+export { default as BoostCard } from "./BoostCard";
+export { default as LeaderboardCard } from "./LeaderboardCard";
+export { default as ToolsCard } from "./ToolsCard";
